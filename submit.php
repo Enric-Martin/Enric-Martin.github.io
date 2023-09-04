ce1f005121a6fc1f07acd31d97062e9854513896
Enric Martin <enricmartin87@gmail.com>
2023-09-04 10:29:35 +0200

    Updating email

--- submit.php
+++ submit.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST["message"];
 
     // Set recipient email address
-    $to = "enricmartin@gmail.com";
+    $to = "enricmartin87@gmail.com";
 
     // Set email subject and headers
     $subject = "Contact Form Submission from $name";
