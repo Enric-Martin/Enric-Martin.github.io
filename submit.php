ab065fc554dd89e27b94d2b5d14630fa5f114e29
Enric Martin <enricmartin87@gmail.com>
2023-09-04 10:06:21 +0200

    Adding the PHP code.

---
+++ submit.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $number = $_POST["number"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    // Set recipient email address
+    $to = "enricmartin@gmail.com";
+
+    // Set email subject and headers
+    $subject = "Contact Form Submission from $name";
+    $headers = "From: $email";
+
+    // Send the email
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Thank you for your message. We will get back to you soon.";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+?>
